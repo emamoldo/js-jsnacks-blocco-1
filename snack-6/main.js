00774db94958c6c8ca67eb43c8b4cd837bcf0b92
emamoldo <moldovanemanuel92.em@gmail.com>
2024-02-26 12:32:02 +0100

    Add the new sctucture for the Snack 6

--- snack-6/main.js
+++ snack-6/main.js
@@ -1,8 +1,21 @@
 // Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
 
-let n = Number(prompt("Write your Numbers"));
-console.log(n);
+/*
+for (let i = 0; i < 3; i++) {
+    const n = Number(prompt("Write your Numbers"));
+    const cubo = n*n*n;
+    console.log(cubo);
+    console.log("Il Cubo del numero è " +cubo);
+}
+*/
 
-let cubo = n*n*n;
+let i=0;
 
-console.log("Il Cubo del numero è " +cubo);
+while (i<3) {
+    const n = Number(prompt("Write your Numbers"));
+    const cubo = n*n*n;
+    console.log(cubo);
+    
+    i++
+    console.log("Il Cubo del numero è " +cubo);
+}
